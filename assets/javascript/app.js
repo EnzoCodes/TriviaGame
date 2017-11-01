fc0754c6c236a412b05e35b780941fab78ef0522
Your Name <enzocnop@gmail.com>
2017-10-31 21:59:55 -0700

    broken progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,29 @@
+var win;
+var loss;
+var userAnswer;
+var answersDisplay;
+var qA_display;
+var input;
+var Uinput;
+var qA;
+var correct = "<img src=assets/images/green.png>";
+var incorrect = "<img src=assets/images/red.png>";
+var count;
+var subButton;
+
+
+// add hide, and show on Start button...
+
 $(document).ready(function() {
-    var win = 0;
-    var loss = 0;
-    var answersDisplay = $("#answersDisplay");
-    var qA_display = $("#question");
+    win = 0;
+    loss = 0;
+    count = 0;
+    holdAnswer = [];
+    answersDisplay = $("#answersList");
+    qA_display = $("#question");
     //Array of OBJECTS...
-    var qA = [
+
+    qA = [
         {
             question: "What color was Washington's white horse?",
             answers: {
@@ -12,7 +31,7 @@ $(document).ready(function() {
                 b: "pinto",
                 c: "white",
                 d: "stalion",
-            }
+            },
             answer: "c",
         },
         {
@@ -22,27 +41,40 @@ $(document).ready(function() {
                 b: "Paul",
                 c: "Chris",
                 d: "David",
-            }
+            },
             answer: "d"
         },
         {
             question: "What is the best bootstrap item?",
-            answers:
+            answers: {
                 a: "form-group",
                 b: "jumbotron",
                 c: "navbar",
                 d: "alert"
-            }
+            },
             answer: "b",
         }
     ];
+
+//var userAnswer = document.quiz.question[i].value;
+// userAnswer.push(holdAnswer) -- not sure if backwards..
+//     document looks in the html
+//         quiz looks in the form.
+//                question[i] looks at the question.
+//                      value looks at the Users's input.
+
+function getAnswer() {
+    Uinput = $('#answerField').value;
+    console.log(Uinput);
+}
+// use an eventlistener for the event
+subButton = $('#subButton');
+$(subButton).on('click', getAnswer);
 
 
 
 
 // Countdown... for some reason it goes negative too...
-
-
 //Build Q/A display with timer...
     function displayMain(){
         // Countdown... for some reason it goes negative too...
@@ -58,20 +90,48 @@ $(document).ready(function() {
             }
         }, 1000);
         // End Countdown Section
+        //Populate Screen
+        function displayQ(){
+            for (var i = 0; i < qA.length; i++) {
+            $("#question").html(qA[i].question);
+            $("#answerList").html()
+        }
 
-        // Populate Screen --
+            //grab formn field value on.click("button")
 
-    }
+            //check against qA[i].answer
+                //Correct condition
 
-
-
-// OnClick of Submit button, do win/loss logic, display image, reset timer
-    function displayResult(){
-
-    }
+                //Incorrect condition
 
 
 
 
+        };
+    };
 
 }); //End document.ready function...
+
+
+// Main problems ==
+// 1. Value from form field is spitting undefined...
+// 2. Can't figure out how to print Array[{object.key{answer}}]
+// 3. Inserting delay into for loop....
+// 4. storing value of form field...
+
+
+
+
+
+// qA[0].question = "what color..."
+// qA[0].answer = "c"
+// qA[1].question = "David's father..."
+// Syntax for looking through Array of Objects...
+
+//
+// for (var i = 0; i < qA.length; i++) {
+//     //generate questions
+//     $("#question").html(qA[0].question);
+//     //generate formn field
+//     $("#answerList").html(qA[0].answers);
+//     //generate button
