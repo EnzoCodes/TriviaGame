5f9824495074c02865e41a2a52d07fab193b2f16
Your Name <enzocnop@gmail.com>
2017-11-02 20:10:48 -0700

    done but terrible

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,9 @@ var subButton;
 var questionList = [];
 var holdAnswer;
 var timeOutText = "You ran out of time!";
+var imageLoss;
+var imageWin;
+var interval;
 
 
 
@@ -62,16 +65,10 @@ $(document).ready(function() {
     ];
 
 
-//var userAnswer = document.quiz.question[i].value;
-// userAnswer.push(holdAnswer) -- not sure if backwards..
-//     document looks in the html
-//         quiz looks in the form.
-//                question[i] looks at the question.
-//                      value looks at the Users's input.
-
     function getAnswer() {
         Uinput = $('#answerField').val();
         console.log(Uinput);
+        console.log(counter);
         // 3 if else WIN -
         // if timer = 0 || submitLetter != answer -- loss
         // you ran out time OR you didnt answer correct...
@@ -79,59 +76,83 @@ $(document).ready(function() {
         if (Uinput === qA[count].answer) {
             win++
             $("#win").text(win);
-            // Also update DOM with image
-            counter = 10
             count++
             $("#progressBarIn").empty('inside');
             $("#quiz").empty();
             $("#question").empty();
-        } else if (counter === 0 || Uinput != qA[count].answer) {
-            loss++
+            displayQ();
+            displayMain();
+            counter = 10;
+        } else if (Uinput != qA[count].answer) {
+            loss++;
             $("#loss").text(loss);
             //Update with loss image
-            count++
-            counter = 10;
+            //Loss image
+            count++;
             $("#progressBarIn").empty('inside');
             $("#quiz").empty();
             $("#question").empty();
+            displayQ();
+            counter = 10;
         }
     };
 
     subButton = $('#subButton');
     $(subButton).on('click', getAnswer);
 
-
-// Countdown... for some reason it goes negative too...
-//Build Q/A display with timer...
-    function displayMain(){
-        //first function, number of times it runs....
-        counter = 10;
-
-        var interval = setInterval(function() {
-            counter--;
-            $("#progressBar").addClass('bar');
-            $("#progressBarIn").addClass('inside');
-            if (counter >= 0) {
-                $("#countDown").text(counter);
-            } if (counter === 0) {
-                clearInterval(counter);
-            }
-        }, 1000);
-        // End Countdown Section
-    }; // End displayMain
-
     function displayQ() {
         var text = (qA[count].question);
         $("#question").html(text);
 
         var aDisplay = (qA[count].answers[0]);
-        for (var x in aDisplay) {
+        for (x in aDisplay) {
             $("#quiz").append(x + ". " + aDisplay[x] + "<br>");
         }
     }; // end DisplayQ
 
-    displayMain();
-    displayQ();
+    // imageWin = setInterval(function(){
+    //     $("#quiz").empty();
+    //     $("#question").empty();
+    //     $("#question").html("<img src='assets/images/green.png'>")
+    // }, 3000);
+    //
+    // imageLoss = setInterval(function(){
+    //     $("#quiz").empty();
+    //     $("#question").empty();
+    //     $("#question").html("<img src='assets/images/red.png'>")
+    // }, 3000);
+
+    // Countdown... for some reason it goes negative too...
+    //Build Q/A display with timer...
+        function displayMain(){
+            //first function, number of times it runs....
+            counter = 10;
+
+            interval = setInterval(function() {
+                counter--;
+                $("#progressBar").addClass('bar');
+                $("#progressBarIn").addClass('inside');
+                if (counter >= 0) {
+                    $("#countDown").text(counter + " s");
+                } if (counter === 0) {
+                    count++;
+                    // clearInterval(counter);
+                    // $("#progressBar").empty('bar');
+                    loss++;
+                    $("#loss").text(loss);
+                    //Update with loss image
+                    //Loss image
+                    $("#progressBarIn").empty('inside');
+                    $("#quiz").empty();
+                    $("#question").empty();
+                    displayQ();
+                    counter = 10;
+                }
+            }, 1000);
+            // End Countdown Section
+        }; // End displayMain
+        displayQ();
+        displayMain();
 });
 
 
